d90c3c57ca98988d898a4ed27716734d593ca1b8
otman-adam <adamotman2005@gmail.com>
2026-03-31 19:31:19 +0200

    css gefixet voor 1 element + filter zoekfunctie toegevoegdt

--- dom.js
+++ dom.js
@@ -3,16 +3,10 @@ let characters = [];
 let favorites = [];
 
 async function fetchCharacters() {
-    // const response = await fetch("https://rickandmortyapi.com/api/character");
-    // const data = await response.json();
-    // characters = data.results;
-    // console.log(characters)
-    // renderCharacters(characters)
-
     fetch("https://rickandmortyapi.com/api/character")
       .then(response => response.json())
       .then(data => {
-        const characters = data.results;
+        characters = data.results;
         console.log(characters);
         renderCharacters(characters);
       })
@@ -23,18 +17,33 @@ async function fetchCharacters() {
 
 const renderCharacters = (characters) => {
     const section = document.getElementById('my-section');
+    section.innerHTML = '';
 
     characters.forEach(character => {
         const div = document.createElement('article');
         div.innerHTML = `
-        <p>${character.name}</p>
+        <a href="#"><img src="${character.image}" alt="Avatar van ${character.name}"></a>
+        <h2>${character.name}</h2>
         <p>${character.status}</p>
         <p>${character.species}</p>
         <p>${character.gender}</p>
         <p>${character.location.name}</p>
-        <p>${character.episode.length} aflevering</p>`;
+        <p>Komt voor in ${character.episode.length} aflevering(en)</p>`;
         section.appendChild(div);
     });
 }
+document.getElementById("zoekbalk").addEventListener('input', (event)=> {
+    const zoekterm = document.getElementById("zoekbalk").value.toLowerCase();
+    const gefilterd = characters.filter(character => {
+        return character.name.toLowerCase().includes(zoekterm) ||
+        character.status.toLowerCase().includes(zoekterm) ||
+        character.species.toLowerCase().includes(zoekterm) ||
+        character.gender.toLowerCase().includes(zoekterm) ||
+        character.location.name.toLowerCase().includes(zoekterm) 
+        // character.episode.length().includes(zoekterm)
+    })
+    renderCharacters(gefilterd)
+})
+
 
 fetchCharacters()
