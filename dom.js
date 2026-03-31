5d9bf01950e5e5460efa350a1013bf719e98e411
otman-adam <adamotman2005@gmail.com>
2026-03-31 18:45:09 +0200

    begin van het css, aapassen van html met nieuwe filters + verbeteren van de dom.js

--- dom.js
+++ dom.js
@@ -3,20 +3,38 @@ let characters = [];
 let favorites = [];
 
 async function fetchCharacters() {
-    const response = await fetch("https://rickandmortyapi.com/api/character")
-        .then(response => {
-            console.log('Status:', response.status);
-            console.log('Status text:', response.statusText);
-            console.log('Headers', response.headers.get('Content-Type'));
-            if (!response.ok) {
-                throw new Error("Characters ophalen is niet gelukt.");
-            }
-            return response.json();
-        })
-        .then(data => console.log("Ontvangen data:", data))
-        .catch(error => console.error("Fout bij het ophalen: ", error))    
+    // const response = await fetch("https://rickandmortyapi.com/api/character");
+    // const data = await response.json();
+    // characters = data.results;
+    // console.log(characters)
+    // renderCharacters(characters)
 
-    const data = await response.json();
-
-    
+    fetch("https://rickandmortyapi.com/api/character")
+      .then(response => response.json())
+      .then(data => {
+        const characters = data.results;
+        console.log(characters);
+        renderCharacters(characters);
+      })
+      .catch(error => {
+        console.error("Er ging iets mis ", error);
+      })
 }
+
+const renderCharacters = (characters) => {
+    const section = document.getElementById('my-section');
+
+    characters.forEach(character => {
+        const div = document.createElement('article');
+        div.innerHTML = `
+        <p>${character.name}</p>
+        <p>${character.status}</p>
+        <p>${character.species}</p>
+        <p>${character.gender}</p>
+        <p>${character.location.name}</p>
+        <p>${character.episode.length} aflevering</p>`;
+        section.appendChild(div);
+    });
+}
+
+fetchCharacters()
