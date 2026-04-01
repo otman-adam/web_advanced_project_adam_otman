c412e253a0dcfeeef5bec5a48c0e4e291af61414
otman-adam <adamotman2005@gmail.com>
2026-04-01 10:21:38 +0200

    js klaar en css verbeterd

--- dom.js
+++ dom.js
@@ -40,10 +40,47 @@ document.getElementById("zoekbalk").addEventListener('input', (event)=> {
         character.species.toLowerCase().includes(zoekterm) ||
         character.gender.toLowerCase().includes(zoekterm) ||
         character.location.name.toLowerCase().includes(zoekterm) 
-        // character.episode.length().includes(zoekterm)
     })
     renderCharacters(gefilterd)
 })
 
+document.getElementById("status").addEventListener('change', (event) => {
+    const value = event.target.value.toLowerCase();
+    const gefilterd = characters.filter(character => {
+        return character.status.toLowerCase().includes(value)
+    })
+    if (value == "alle status") { 
+        renderCharacters(characters) 
+    }
+    else {
+        renderCharacters(gefilterd)
+    }
+})
+
+document.getElementById("sortAZ").addEventListener('change', (event) => {
+    const value = event.target.value.toLowerCase();
+    let gefilterd = [];
+    if (value == "naam a-z") {
+        gefilterd = [...characters].sort((a, b) => a.name.localeCompare(b.name));
+    } else if (value == "naam z-a") {
+        gefilterd = [...characters].sort((a, b) => b.name.localeCompare(a.name));
+    } else {
+        gefilterd = [...characters];
+    }
+    renderCharacters(gefilterd)
+})
+
+document.getElementById("afkomst").addEventListener('change', (event) => {
+    const value = event.target.value.toLowerCase();
+    let gefilterd = [];
+    if (value == "alle") {
+        gefilterd = characters;
+    } else {
+        gefilterd = characters.filter(character => {
+            return character.location.name.toLowerCase().includes(value)
+        })
+    }
+    renderCharacters(gefilterd);
+})
 
 fetchCharacters()
